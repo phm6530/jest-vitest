aa0709e0bfe793e170375548ba1929557a8f5506
hm <squirrel309@naver.com>
2025-06-16 17:32:56 +0900

    tdd

--- src/pages/Calculator/Calculator.test.tsx
+++ src/pages/Calculator/Calculator.test.tsx
@@ -5,8 +5,8 @@ import userEvent from "@testing-library/user-event";
 // userEvent - 브라우저 상호작용 유틸
 // screen - tag
 // type - 행위
-// textHaveContent - 초기값에 대한 검증
-// tobe 변경 값에 대한 검증
+// textHaveContent - 포함하였는지에 대한 검증
+// tobe - 명확하게 일치하는지 검증
 
 describe("계산기", () => {
   // 초기값 0
@@ -17,7 +17,7 @@ describe("계산기", () => {
   });
 
   //Insert & slice
-  test("값 insert & slice", async () => {
+  test("insert & slice", async () => {
     render(<Calculator />);
     const result = screen.getByTestId("result");
     const numbersToClick = ["9", "9", "1"];
